3bfd67e15b1c459dc81bd4572f9a31ef6204d8ef
tuhinpal <tkp03463@gmail.com>
2022-04-10 10:11:31 +0530

    Added rewind and forward button

--- src/helpers/plyrOptions.js
+++ src/helpers/plyrOptions.js
@@ -12,6 +12,19 @@ export default function plyrOptions(configs) {
     invertTime: true,
     seekTime: 10,
     storage: { enabled: true, key: "tplayerjs" },
+    controls: [
+      "play-large", // The large play button in the center
+      "rewind", // Rewind by the seek time (default 10 seconds)
+      "play", // Play/pause playback
+      "fast-forward", // Fast forward by the seek time (default 10 seconds)
+      "progress", // The progress bar and scrubber for playback and buffering
+      "current-time", // The current time of playback
+      "mute", // Toggle mute
+      "volume", // Volume control
+      "captions", // Toggle captions
+      "settings", // Settings menu
+      "fullscreen", // Toggle fullscreen
+    ],
   };
 
   return options;
